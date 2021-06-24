e8874f21b83b8b8ce6039e227dfda5f210759bc4
shantanu bhuruk <shantanu.bhuruk@techbulls.com>
2021-06-24 19:36:44 +0530

    added hello.java file

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello
+{
+    public static void main(final String[] array) {
+        for (int i = 0; i < 10; ++i) {
+            System.out.println("Hello World... " + i);
+        }
+    }
+}
